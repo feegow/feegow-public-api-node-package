7c2ab30f7d3e4b731694343fd38edbc1f1abafd2
GustavoContreiras-Feegow <82453798+GustavoContreiras-Feegow@users.noreply.github.com>
2021-07-22 10:32:24 -0300

    Update FeegowPublicAPI.test.js

--- src/FeegowPublicAPI.test.js
+++ src/FeegowPublicAPI.test.js
@@ -4,13 +4,13 @@ var feegowApi;
 
 test('Authenticate', () => {
 
-    feegowApi = new FeegowPublicAPI('eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJmZWVnb3ciLCJhdWQiOiJwdWJsaWNhcGkiLCJpYXQiOjE2MjUwNjk5MTAsImxpY2Vuc2VJRCI6IjU0NTkiLCJzdWNjZXNzIjp0cnVlLCJ1c2VySWQiOiIzMDAwMjgiLCJsaWNlbnNlSWQiOiI1NDU5IiwiaXNIb21vbG9nIjp0cnVlLCJkYXRldGltZSI6IjIwMjEtMDYtMzAiLCJ2YWxpZF91bnRpbCI6IjIwMjEtMDctMDEifQ.PWj6gc78t1WzidUyp0lTOIizQhty5M9KkKwGvjMWn6w');
+    feegowApi = new FeegowPublicAPI('');
 
     const tokenInfo = feegowApi.tokenInfo;
 
-    expect(tokenInfo).toHaveProperty('iss');
-    expect(tokenInfo).toHaveProperty('aud');
-    expect(tokenInfo).toHaveProperty('iat');
+    expect(tokenInfo).not.toHaveProperty('iss');
+    expect(tokenInfo).not.toHaveProperty('aud');
+    expect(tokenInfo).not.toHaveProperty('iat');
 });
 
 test('Get all patients', async () => {
